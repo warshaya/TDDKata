ab94d849d1621dfa7dbafbffd724a523b1db8201
warshaya <a.warshay@techsmith.com>
2017-09-06 09:19:16 -0400

    got custom delimiters working

--- 2017-09-04/python/StringCalculator.py
+++ 2017-09-04/python/StringCalculator.py
@@ -1,10 +1,18 @@
 class StringCalculator:
 
-   #def __init__(self):
+#   def __init__(self):
+
 
    def add( self, str ):
       if ( str == '' ):
          return 0
+      if ( str.startswith('//') ):
+         str = str.split('//')[1]
+         str = str.split('\n', 1)
+         str[1] = str[1].replace( str[0], ',')
+         str = str[1]
+      if ( str.find('\n') ):
+         str = str.replace('\n', ',')
       if ( str.find(',') ):
          values = str.split(',')
          result = 0
--- 2017-09-04/python/StringCalculatorTest.py
+++ 2017-09-04/python/StringCalculatorTest.py
@@ -3,37 +3,49 @@ import StringCalculator
 
 class StringCalculatorTest(unittest.TestCase):
 
-   def test_Add_EmptyString_ReturnsZero(self):
+   def testAdd_EmptyString_ReturnsZero(self):
       testCalc = StringCalculator.StringCalculator()
       userInput = ""
       result = testCalc.add( userInput )
       self.assertEqual( result, 0 )
 
-   def test_Add_NumberOne_ReturnsOne(self):
+   def testAdd_NumberOne_ReturnsOne(self):
       testCalc = StringCalculator.StringCalculator()
       userInput = '1'
       result = testCalc.add( userInput )
       self.assertEqual( result, 1 )
 
-   def test_Add_NumberTwo_ReturnsTwo(self):
+   def testAdd_NumberTwo_ReturnsTwo(self):
       result = StringCalculator.StringCalculator().add( '2' )
       self.assertEqual( result, 2 )
 
-   def test_Add_ZerosInFront_ReturnsSameNumber(self):
+   def testAdd_ZerosInFront_ReturnsSameNumber(self):
       result = StringCalculator.StringCalculator().add('000016')
       self.assertEqual( result, 16 )
 
-   def test_Add_TwoOnesSeparatedWithComma_ReturnsSum(self):
+   def testAdd_TwoOnesSeparatedWithComma_ReturnsSum(self):
       result = StringCalculator.StringCalculator().add('1,1')
       self.assertEqual( result, 2 )
 
-   def test_Add_TwoDifferentNumbers_ReturnsSum(self):
+   def testAdd_TwoDifferentNumbers_ReturnsSum(self):
       result = StringCalculator.StringCalculator().add('23,18')
       self.assertEqual( result, 41 )
 
-   def test_Add_FiveNumbers_ReturnsSum(self):
+   def testAdd_FiveNumbers_ReturnsSum(self):
       result = StringCalculator.StringCalculator().add('1,2,3,4,5')
       self.assertEqual( result, 15 )
+
+   def testAdd_NewlineSeparator_ReturnsSum(self):
+      result = StringCalculator.StringCalculator().add('1\n2,3')
+      self.assertEqual( result, 6 )
+
+   def testAdd_CustomDelimiterSingleNumber_ReturnsSameNumber(self):
+      result = StringCalculator.StringCalculator().add('//>\n1>2')
+      self.assertEqual( result, 3 )
+
+   def testAdd_DifferentCustomDelimiter_ReturnsSum(self):
+      result = StringCalculator.StringCalculator().add('//;><\n1;><2\n3,4')
+      self.assertEqual( result, 10 )
 
 if __name__ == '__main__':
     unittest.main()
