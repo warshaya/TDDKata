86dbdf3e1d1bf632f8682d9127fa82b72f555551
warshaya <a.warshay@techsmith.com>
2017-09-08 11:49:07 -0400

    change to use stack instead of heap

--- 2017-09-01/StringCalculator/StringCalculatorTestRunner/StringCalculatorTests.cpp
+++ 2017-09-01/StringCalculator/StringCalculatorTestRunner/StringCalculatorTests.cpp
@@ -11,105 +11,105 @@
 
 TEST( StringCalculator, Add_EmptyString_ReturnsZero )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 0 );
 }
 
 TEST( StringCalculator, Add_One_ReturnsOne )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "1";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 1 );
 }
 
 TEST( StringCalculator, Add_Two_ReturnsTwo )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "2";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 2 );
 }
 
 TEST( StringCalculator, Add_ArbitrarySingleNumber_ReturnsSameNumber )
 {
-   StringCalculator * myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "929508271";                   // 9 digits are too many
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 929508271 );
 }
 
 TEST( StringCalculator, Add_ZerosInFront_ReturnsSameNumber )
 {
-   StringCalculator * myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "000000089";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 89 );
 }
 
 TEST( StringCalculator, Add_TwoOnes_ReturnsTwo )
 {
-   StringCalculator * myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "1,1";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 2 );
 }
 
 TEST( StringCalculator, Add_OneAndTwo_ReturnsThree )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "1,2";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 3 );
 }
 
 TEST( StringCalculator, Add_TwoRandomNumbers_ReturnsSum )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "432,68";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 500 );
 }
 
 TEST( StringCalculator, Add_ThreeOnes_ReturnsThree )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "1,1,1";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 3 );
 }
 
 TEST( StringCalculator, Add_ThreeRandomNumbers_ReturnsTheSum )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "346,97,4056";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 4499 );
 }
 
 TEST( StringCalculator, Add_TwelveNumbers_ReturnsTheSum )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "0,1,2,3,4,5,6,7,8,9,10,11";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 66 );
 }
 
 TEST( StringCalculator, Add_NewlineDelmiterOnes_ReturnsTwo )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "1\n1";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 2 );
 }
 
 TEST( StringCalculator, Add_NewlineAndCommaMixed_ReturnsSum )
 {
-   StringCalculator *myCalc = new StringCalculator;
+   StringCalculator myCalc = StringCalculator();
    std::string input = "2,4\n5";
-   int result = myCalc->add( input );
+   int result = myCalc.add( input );
    EXPECT_EQ( result, 11 );
 }
 
