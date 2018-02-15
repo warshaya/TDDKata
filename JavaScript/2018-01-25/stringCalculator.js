44b92eac43c95a5d2ae9fc9d660cf121a7e5037d
warshaya <warshaya@gmail.com>
2018-02-15 09:43:23 -0800

    add ability to use custom delimiter, refactoring for let, refactor for readibility

--- JavaScript/2018-01-25/stringCalculator.js
+++ JavaScript/2018-01-25/stringCalculator.js
@@ -2,28 +2,57 @@
 function StringCalculator() {
 
   this.add = function (input) {
+    input = cleanInput(input);
     if (isEmptyString(input)) {
       return 0;
     } else if (containsSingleValue(input)) {
       return Number.parseInt(input);
     } else {
-      input = input.replace('\n', ',');
-      var values = input.split(',');
-      var total = 0;
-      for (var i = 0; i < values.length; i++) {
+      let values = input.split(',');
+      let total = 0;
+      let i;
+      let stop = values.length;
+      for (i = 0; i < stop; i++) {
         total += Number.parseInt(values[i]);
       }
       return total;
     }
   }
 
-  function isEmptyString(input) {
+  function cleanInput (input) {
+    if (containsCustomDelimiter(input)) {
+      let endOfPrefixIndex = input.indexOf('\n');
+      let prefix = input.substring(input.indexOf('//') + 2, endOfPrefixIndex);
+      input = input.substring(endOfPrefixIndex + 1);
+      let customDelimiter = new RegExp(prefix, 'g');
+      input = input.replace(customDelimiter, ',');
+    }
+    if (containsNewline(input)) {
+      input = input.replace(/\n/g, ',');
+    }
+    return input;
+  }
+
+  function isEmptyString (input) {
     return (input == "");
   }
 
-  function containsSingleValue(input) {
-    return (input.indexOf(',') < 0 && input.indexOf('\n') < 0);
+  function containsSingleValue (input) {
+    return !(containsComma(input));
   }
+
+  function containsComma (input) {
+    return (input.indexOf(',') >= 0);
+  }
+
+  function containsNewline (input) {
+    return (input.indexOf('\n') >= 0);
+  }
+
+  function containsCustomDelimiter (input) {
+    return (input.indexOf('//') == 0);
+  }
+
 }
 
 module.exports = StringCalculator;
--- JavaScript/2018-01-25/stringCalculator.test.js
+++ JavaScript/2018-01-25/stringCalculator.test.js
@@ -6,71 +6,99 @@ test('add_emptyString_returnsZero', () => {
 });
 
 test('add_oneZero_returnsZero', () => {
-  var result = new StringCalculator().add("0");
+  let result = new StringCalculator().add("0");
   expect(result).toBe(0);
 });
 
 test('add_threeZeros_returnsZero', () => {
-  var result = new StringCalculator().add("000");
+  let result = new StringCalculator().add("000");
   expect(result).toBe(0);
 });
 
 test('add_singleOne_returnsOne', () => {
-  var result = new StringCalculator().add("1");
+  let result = new StringCalculator().add("1");
   expect(result).toBe(1);
 });
 
 test('add_singleLargeNumber_returnsItself', () => {
-  var result = new StringCalculator().add("87654");
+  let result = new StringCalculator().add("87654");
   expect(result).toBe(87654);
 });
 
 test('add_twoZeros_returnsZero', () => {
-  var result = new StringCalculator().add("0,0");
+  let result = new StringCalculator().add("0,0");
   expect(result).toBe(0);
 });
 
 test('add_twoOnes_returnsTwo', () => {
-  var result = new StringCalculator().add("1,1");
+  let result = new StringCalculator().add("1,1");
   expect(result).toBe(2);
 });
 
 test('add_twoDifferentNumbers_ReturnsTheSum', () => {
-  var result = new StringCalculator().add("1,23");
+  let result = new StringCalculator().add("1,23");
   expect(result).toBe(24);
 });
 
 test('add_threeOnes_ReturnsThree', () => {
-  var result = new StringCalculator().add("1,1,1");
+  let result = new StringCalculator().add("1,1,1");
   expect(result).toBe(3);
 });
 
 test('add_manyNumbers_ReturnsTheSum', () => {
-  var result = new StringCalculator().add("1,10,100,1000,10000,100000,1000000");
+  let result = new StringCalculator().add("1,10,100,1000,10000,100000,1000000");
   expect(result).toBe(1111111);
 });
 
 test('add_randomNumberOfIntegersLessThan1000_ReturnsTheSum', () => {
-  var inputs = [];
-  var size = Math.floor(Math.random() * 100);
-  for (var i = 0; i < size; i++) {
+  let inputs = [];
+  let i;
+  let size = Math.floor(Math.random() * 100);
+  for (i = 0; i < size; i++) {
     inputs.push(Math.floor(Math.random() * 1000));
   }
-  var total = 0;
-  for (var i = 0; i < inputs.length; i++) {
-    total += inputs[i];
+  let total = 0;
+  let j;
+  let stop = inputs.length;
+  for (j = 0; j < stop; j++) {
+    total += inputs[j];
   }
-  var inputString = inputs.toString();
-  var result = new StringCalculator().add(inputString);
+  let inputString = inputs.toString();
+  let result = new StringCalculator().add(inputString);
   expect(result).toBe(total);
 });
 
 test('add_numbersSeparatedByNewlineAndComma_ReturnsSum', () => {
-  var result = new StringCalculator().add("1\n2,3");
+  let result = new StringCalculator().add("1\n2,3");
   expect(result).toBe(6);
 });
 
 test('add_numbersSeparatedByNewlines_ReturnsSum', () => {
-  var result = new StringCalculator().add("1\n1");
+  let result = new StringCalculator().add("1\n1");
   expect(result).toBe(2);
+});
+
+test('add_manyNumbersAlternatingSeparators_ResturnsSum', () => {
+  let result = new StringCalculator().add("1,1\n2,2\n3,3");
+  expect(result).toBe(12);
+});
+
+test('add_manyNumbersUsingNewlines_ReturnsSum', () => {
+  let result = new StringCalculator().add("1\n2\n3\n4\n5\n6");
+  expect(result).toBe(21);
+});
+
+test('add_customDelimiterSingleNumber_ReturnsSameNumber', () => {
+  let result = new StringCalculator().add("//;\n1");
+  expect(result).toBe(1);
+});
+
+test('add_customDelimiterTwoNumbers_ReturnsTheirSum', () => {
+  let result = new StringCalculator().add("//;\n1;2");
+  expect(result).toBe(3);
+});
+
+test('add_customDelimieterThreeNumbers_ReturnsTheirSum', () => {
+  let result = new StringCalculator().add("//;\n1;1;1");
+  expect(result).toBe(3);
 });
