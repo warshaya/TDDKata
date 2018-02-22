4e94b743debbc20e2077c859754c5b8c76fa32d3
warshaya <warshaya@gmail.com>
2018-02-22 07:53:41 -0800

    rename stringCalculatorFactory -> stringCalculator

--- JavaScript/2018-02-20/stringCalculator.test.js
+++ JavaScript/2018-02-20/stringCalculator.test.js
@@ -1,31 +1,31 @@
 "use strict";
 
-const stringCalculatorFactory = require('./stringCalculator');
+const stringCalculator = require('./stringCalculator');
 
 test('factory_createsAStringCalculator', () => {
-  let calc = stringCalculatorFactory();
+  let calc = stringCalculator();
   expect(calc).toBeDefined();
 });
 
 test('stringCalculator_containsAddMethod', () => {
-  let calc = stringCalculatorFactory();
+  let calc = stringCalculator();
   expect(calc.add).toBeDefined();
 });
 
 test('add_emptyString_returnsZero', () => {
-  let calc = stringCalculatorFactory();
+  let calc = stringCalculator();
   let result = calc.add("");
   expect(result).toBe(0);
 });
 
 test('add_zero_returnsZero', () => {
-  let calc = stringCalculatorFactory();
+  let calc = stringCalculator();
   let result = calc.add("0");
   expect(result).toBe(0);
 });
 
 test('add_one_returnsOne', () => {
-  let calc = stringCalculatorFactory();
+  let calc = stringCalculator();
   let result = calc.add("1");
   expect(result).toBe(1);
 });
