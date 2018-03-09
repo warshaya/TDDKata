52b17a92bfb2db5fdd54f2ee6c76c5079cf0c785
warshaya <warshaya@gmail.com>
2018-03-09 09:48:51 -0800

    +add method, returns 0 on empty string, or returns single num

--- JavaScript/2018-03-08/stringCalculator.js
+++ JavaScript/2018-03-08/stringCalculator.js
@@ -3,11 +3,19 @@ let stringCalculator;
 (function () {
 
   let proto = {
+
+    add: function (input) {
+      if (input == '') {
+        return 0;
+      } else {
+        return Number.parseInt(input, 10);
+      }
+    }
  
   }
 
   stringCalculator = function () {
-    Object.create(proto);
+    return Object.create(proto);
   }
 
 })();
--- JavaScript/2018-03-08/stringCalculator.test.js
+++ JavaScript/2018-03-08/stringCalculator.test.js
@@ -1,5 +1,13 @@
-const stringCalculator = require('./stringCalculator');
+const stringCalculator = require('./stringCalculator.js');
 
 test('stringCalculator_isDefined', () => {
   expect(new stringCalculator()).toBeDefined();
+});
+
+test('add_emptyString_returnsZero', () => {
+  expect(new stringCalculator().add('')).toBe(0);
+});
+
+test('add_singleNumber_returnsTheNumber', () => {
+  expect(new stringCalculator().add('1')).toBe(1);
 });
