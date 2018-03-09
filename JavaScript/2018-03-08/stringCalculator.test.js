43aa34c43ad4a7bc64581dbd647e9408dd3783e3
warshaya <warshaya@gmail.com>
2018-03-09 09:59:02 -0800

    add takes an indefinite number of nums

--- JavaScript/2018-03-08/stringCalculator.test.js
+++ JavaScript/2018-03-08/stringCalculator.test.js
@@ -16,3 +16,7 @@ test('add_twoNums_returnsTheSum', () => {
   expect(new stringCalculator().add('2,1')).toBe(3);
 });
 
+test('add_multipleNumbs_returnsTheSum', () => {
+  expect(new stringCalculator().add('3,4,5,6,7,8,10')).toBe(43);
+});
+
