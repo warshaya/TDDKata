92a651b1a8fe729d4cdeed50d6fb46a9c7d7bb8c
warshaya <>
2023-03-06 12:46:20 -0800

    refactor: bash- parse and add 2 numbers

--- bash/2023-03-06/string_calculator.sh
+++ bash/2023-03-06/string_calculator.sh
@@ -8,6 +8,25 @@ add () {
   then
     add_result=0
     r_val=0
+  else
+    echo "LOG: value found" 1>&2
+    check_if_comma=$(echo $1 | grep ",")
+    if [[ "$check_if_comma" == "" ]]
+    then
+      echo "LOG: result of grep: $check_if_comma   and return value: $?" 1>&2
+      add_result=$1
+      r_val=0
+    else
+      echo "LOG: found a comma in the input string" 1>&2
+      # split and sum
+      numbers=$(echo "$1" | tr "," " ")
+      echo "LOG: found these numbers: $numbers" 1>&2
+      # from https://unix.stackexchange.com/questions/544926/bash-sum-numbers-in-a-variable
+      # also https://tldp.org/LDP/abs/html/string-manipulation.html
+      arr=( $numbers )
+      add_result="$((${arr[@]/%/+}0))"
+      r_val=0
+    fi
   fi
   echo $add_result
   return $r_val
--- bash/2023-03-06/test_string_calculator.sh
+++ bash/2023-03-06/test_string_calculator.sh
@@ -19,8 +19,29 @@ test_add () {
     r_val="1"
   fi
   echo "r_val: $r_val"
+  echo
   return "$r_val"
 }
 
+accumulated_results=0
+
+#### test cases here #####
+
+
 test_add "" "0"
+accumulated_results=$((accumulated_results + $?))
+
+test_add "1" "1"
+accumulated_results=$((accumulated_results + $?))
+
+test_add "1,2" "3"
+accumulated_results=$((accumulated_results + $?))
+
+test_add "0,2" "2"
+accumulated_results=$((accumulated_results + $?))
+
+#### end test cases here ####
+
+echo "accumulated_results: $accumulated_results"
+exit $accumulated_results
 
